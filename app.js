87e586da34d67210b602321d62a28394f267b8b1
Rodriguez Rivero Daniel <danierod@gemalto.com>
2016-05-15 10:59:16 +0200

    Fixed bad users reference

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var mongoose = require('mongoose');
 
